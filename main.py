8f21aceef52c563df52a314f40341caf47ba6304
Hariel <hariel@hotmail.com>
2023-03-07 10:24:03 -0300

    feat: main = get_Ifs

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
+import db
+
+database = db.get_database()
 
 def get_IFs():
     
@@ -6,47 +9,72 @@ def get_IFs():
     
     try:
         result = requests.get(url, verify=False)
-        return result.json()['value']
+
+        for data in result.json()['value']:
+            bank = database.banks.find_one({'CnpjInstituicao': data['CnpjInstituicao']})
+
+            print("bank (data): ", data['NomeInstituicao'])
+
+            if bank: 
+                print("bank encontrado: ", bank['NomeInstituicao'])
+
+                recursoAlreadyExist = next(item for item in bank['apis'] if item['Recurso'] == data['Recurso'])
+
+                if recursoAlreadyExist: 
+                    print("recurso já existe: ", recursoAlreadyExist['Recurso'])
+                    break
+
+                database.banks.update_one(bank, {'$push': {'apis':  {
+                    'Api': data['Api'], 'Versao': data['Versao'], 'Recurso': data['Recurso'], 'Argumento': data['Argumento'], 'Situacao': data['Situacao'], 'URLDados': data['URLDados'], 'URLConsulta': data['URLConsulta']
+                }}})
+            else:
+                print("bank sendo criado: ", bank['NomeInstituicao'])
+
+                database.banks.insert_one({'CnpjInstituicao': data['CnpjInstituicao'], 'NomeInstituicao': data['NomeInstituicao'], 'apis': []})
+
+            # return
     except Exception as e:
         print(e)
- 
-def get_tariffs_by_if(if_cnpj, tipo_pessoa):
+
+# def get_tariffs_by_if(if_cnpj, tipo_pessoa):
     
-    pessoa = 'F' if tipo_pessoa == 'pf' else 'J'
-    # cnpj = if_cnpj[:9]
-    cnpj = if_cnpj[:8]
+#     pessoa = 'F' if tipo_pessoa == 'pf' else 'J'
+#     # cnpj = if_cnpj[:9]
+#     cnpj = if_cnpj[:8]
     
-    url = f"https://olinda.bcb.gov.br/olinda/servico/Informes_ListaTarifasPorInstituicaoFinanceira/versao/v1/odata/ListaTarifasPorInstituicaoFinanceira(PessoaFisicaOuJuridica=@PessoaFisicaOuJuridica,CNPJ=@CNPJ)?@PessoaFisicaOuJuridica='{pessoa}'&@CNPJ='{cnpj}'&$top=10000&$format=json&$select=CodigoServico,Servico,Unidade,DataVigencia,ValorMaximo,TipoValor,Periodicidade"
+#     url = f"https://olinda.bcb.gov.br/olinda/servico/Informes_ListaTarifasPorInstituicaoFinanceira/versao/v1/odata/ListaTarifasPorInstituicaoFinanceira(PessoaFisicaOuJuridica=@PessoaFisicaOuJuridica,CNPJ=@CNPJ)?@PessoaFisicaOuJuridica='{pessoa}'&@CNPJ='{cnpj}'&$top=10000&$format=json&$select=CodigoServico,Servico,Unidade,DataVigencia,ValorMaximo,TipoValor,Periodicidade"
     
-    try:
-        result = requests.get(url)
+#     try:
+#         result = requests.get(url)
         
-        if len(result.json()['value']) > 0:
-            print(cnpj, result.status_code, result.json()['value'])
+#         if len(result.json()['value']) > 0:
+#             print()
+#             # print(cnpj, result.status_code, result.json()['value'])
         
-    except Exception as e:
-        print(e)
+#     except Exception as e:
+#         print(e)
         
 def run_test():
     
     ifs = get_IFs()
     
-    ifs_nomes = []
+#     ifs_nomes = []
     
-    for i in range(len(ifs)):
-        cnpj = ifs[i]['CnpjInstituicao']
-        res = get_tariffs_by_if(cnpj, 'pf')
-        print(res)
-        break
-        # print(ifs[])
-        # if ifs[i]['NomeInstituicao'] not in ifs_nomes:
-        #     ifs_nomes.append(ifs[i]['NomeInstituicao'])
+#     for i in range(len(ifs)):
+#         cnpj = ifs[i]['CnpjInstituicao']
+#         res = get_tariffs_by_if(cnpj, 'pf')
+#         # print(res)
+#         break
+#         # print(ifs[])
+#         # if ifs[i]['NomeInstituicao'] not in ifs_nomes:
+#         #     ifs_nomes.append(ifs[i]['NomeInstituicao'])
       
-    print(len(ifs_nomes))  
+#     # print(len(ifs_nomes))  
     
-    for i in range(len(ifs_nomes)):
-        print(ifs_nomes[i])
+#     # for i in range(len(ifs_nomes)):
+#         # print(ifs_nomes[i])
         
         
 if __name__ == "__main__":
     run_test()
+
