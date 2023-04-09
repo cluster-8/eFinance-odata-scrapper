a0307a82d961a9175061c9b0d48bf427632c752d
vinicius-hso <viniciushso@icloud.com>
2023-04-08 21:50:07 -0300

    Feat: add tariff min value and refactoring

--- olinda.py
+++ olinda.py
@@ -25,10 +25,11 @@ def get_physical_person_tariffs(instituition_cnpj: str):
     '''
     try:
         cnpj = instituition_cnpj[:8]
-    
-        url = f"https://olinda.bcb.gov.br/olinda/servico/Informes_ListaTarifasPorInstituicaoFinanceira/versao/v1/odata/ListaTarifasPorInstituicaoFinanceira (PessoaFisicaOuJuridica=@PessoaFisicaOuJuridica,CNPJ=@CNPJ)?@PessoaFisicaOuJuridica='F'&@CNPJ='{cnpj}'&$top=10000&$format=json&$select=CodigoServico,Servico,Unidade,DataVigencia,ValorMaximo,TipoValor,Periodicidade"
+        
+        url = f"https://olinda.bcb.gov.br/olinda/servico/Informes_ListaTarifasPorInstituicaoFinanceira/versao/v1/odata/ListaTarifasPorInstituicaoFinanceira(PessoaFisicaOuJuridica=@PessoaFisicaOuJuridica,CNPJ=@CNPJ)?@PessoaFisicaOuJuridica='F'&@CNPJ='{cnpj}'&$top=10000&$format=json&$select=CodigoServico,Servico,Unidade,DataVigencia,ValorMaximo,TipoValor,Periodicidade"
         
         result = requests.get(url)
+        print(cnpj, result)
         
         if len(result.json()['value']) > 0: 
             return result.json()['value']
@@ -44,8 +45,8 @@ def get_juridical_person_tariffs(instituition_cnpj: str):
     '''
     try:
         cnpj = instituition_cnpj[:8]
-    
-        url = f"https://olinda.bcb.gov.br/olinda/servico/Informes_ListaTarifasPorInstituicaoFinanceira/versao/v1/odata/ListaTarifasPorInstituicaoFinanceira (PessoaFisicaOuJuridica=@PessoaFisicaOuJuridica,CNPJ=@CNPJ)?@PessoaFisicaOuJuridica='J'&@CNPJ='{cnpj}'&$top=10000&$format=json&$select=CodigoServico,Servico,Unidade,DataVigencia,ValorMaximo,TipoValor,Periodicidade"
+        
+        url = f"https://olinda.bcb.gov.br/olinda/servico/Informes_ListaTarifasPorInstituicaoFinanceira/versao/v1/odata/ListaTarifasPorInstituicaoFinanceira(PessoaFisicaOuJuridica=@PessoaFisicaOuJuridica,CNPJ=@CNPJ)?@PessoaFisicaOuJuridica='J'&@CNPJ='{cnpj}'&$top=100&$format=json&$select=CodigoServico,Servico,Unidade,DataVigencia,ValorMaximo,TipoValor,Periodicidade"
         
         result = requests.get(url)
         
@@ -54,7 +55,6 @@ def get_juridical_person_tariffs(instituition_cnpj: str):
         
     except Exception as e:
         print("Get Olinda Juridical Person Services Tariffs error:", e)
-
         
 def get_tariffs_by_cnpj(cnpj: str, instituition_name: str):
     '''
@@ -95,23 +95,55 @@ def get_tariffs_by_cnpj(cnpj: str, instituition_name: str):
     except Exception as e:
         print(f"Get tariffs by cnpj error: {e}")
         
-def get_financial_instituitions_endpoints():
-    try:
-        url = "https://olinda.bcb.gov.br/olinda/servico/DASFN/versao/v1/odata/Recursos?$top=10000&&$filter=Api%20eq%20'taxas_cartoes'&$format=json&$select=Api,Versao,CnpjInstituicao,NomeInstituicao,Recurso,Argumento,Situacao,URLDados"
-        
-        response = requests.get(url)
-        
-        return response.json()['value']
-    except Exception as e:
-        print(f"Get Financial Instituitions endpoints error: {e}")
-
+def get_consolidated_groups():
     '''
+    Returns the list of consolidated groups from Olinda Source.
     '''
     try:
-        url = "https://olinda.bcb.gov.br/olinda/servico/taxaJuros/versao/v2/odata/ConsultaDatas?$top=10000&$format=json&$select=inicioPeriodo,fimPeriodo,tipoModalidade"
+        url = "https://olinda.bcb.gov.br/olinda/servico/Informes_ListaTarifasPorInstituicaoFinanceira/versao/v1/odata/GruposConsolidados?$top=100&$format=json&$select=Codigo,Nome"
         
-        response = requests.get(url)
-        
-        return response.json()['value']
+        groups = requests.get(url).json()['value']
+        return groups
     except Exception as e:
-        print(f"Get Rates on Credits Operations error: {e}")
+        print(f"Get Consolidated Groups error: {e}")
+        
+def get_financial_instituitions_by_group(group_code: str):
+    '''
+    Returns the list of Financial Instituitions of a given group code from Olinda Source.
+    '''
+    try:
+        url = f"https://olinda.bcb.gov.br/olinda/servico/Informes_ListaTarifasPorInstituicaoFinanceira/versao/v1/odata/ListaInstituicoesDeGrupoConsolidado(CodigoGrupoConsolidado=@CodigoGrupoConsolidado)?@CodigoGrupoConsolidado='{group_code}'&$top=10000&$format=json&$select=Cnpj,Nome"
+        
+        instituitions = requests.get(url).json()['value']
+        return instituitions
+    except Exception as e:
+        print(f"Get Financial Instituitions by Consolidated Group error: {e}")
+        
+def get_all_services():
+    '''
+    Returns the list of all services from Olinda Source.
+    '''
+    try:
+        url = "https://olinda.bcb.gov.br/olinda/servico/Informes_ListaTarifaPorValores/versao/v1/odata/ServicosBancarios?$top=10000&$format=json&$select=Codigo,Nome"
+        
+        services = requests.get(url).json()['value']
+        return services
+    except Exception as e:
+        print(f"Get All Services from Olinda Source error: {e}")
+        
+def get_all_services_values_by_group(service_type: str, group_code: str):
+    '''
+    Returns a list of max, min and mean values for all physical person services of a given group.
+    
+    :param group_code: str
+    :param service_type: str
+    '''
+    try:
+        if service_type not in "FJ": raise Exception(f"Inavlid service type: {service_type}")
+        
+        url = f"https://olinda.bcb.gov.br/olinda/servico/Informes_ListaValoresDeServicoBancario/versao/v1/odata/ListaValoresServicoBancario(PessoaFisicaOuJuridica=@PessoaFisicaOuJuridica,CodigoGrupoConsolidado=@CodigoGrupoConsolidado)?@PessoaFisicaOuJuridica='{service_type}'&@CodigoGrupoConsolidado='{group_code}'&$top=10000&$format=json&$select=NomeServico,ValorMinimo,PeriodicidadeValorMinimo,ValorMaximo,PeriodicidadeValorMaximo,ValorMedio"
+        
+        values = requests.get(url).json()['value']
+        return values
+    except Exception as e:
+        print(f"get All Services Values by Group error: {e}")
