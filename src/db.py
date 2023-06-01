c7fefce31fcbdfc54e1aefbd03f0eb0f30d7747a
Hariel <hariel@hotmail.com>
2023-06-01 11:16:30 -0300

    feat: db - env values

--- src/db.py
+++ src/db.py
@@ -1,21 +1,22 @@
 from dotenv import dotenv_values
+import os
 import psycopg2
 import logging
 
-envs = dotenv_values('.env')
+envs = dotenv_values('./.env')
 
-print(envs['DB_HOST'])
-print(envs['DB_NAME'])
-print(envs['DB_USER'])
-print(envs['DB_PASS'])
+print((os.getenv('DB_HOST') if os.getenv('DB_HOST') else envs['DB_HOST']))
+print((os.getenv('DB_NAME') if os.getenv('DB_NAME') else envs['DB_NAME']))
+print((os.getenv('DB_USER') if os.getenv('DB_USER') else envs['DB_USER']))
+print((os.getenv('DB_HOST') if os.getenv('DB_HOST') else envs['DB_HOST']))
 
 def get_database_psql():
    try:
       conn = psycopg2.connect(
-         host=envs['DB_HOST'],
-         database=envs['DB_NAME'],
-         user=envs['DB_USER'],
-         password=envs['DB_PASS'])
+         host=(os.getenv('DB_HOST') if os.getenv('DB_HOST') else envs['DB_HOST']),
+         database=(os.getenv('DB_NAME') if os.getenv('DB_NAME') else envs['DB_NAME']),
+         user=(os.getenv('DB_USER') if os.getenv('DB_USER') else envs['DB_USER']),
+         password=(os.getenv('DB_PASS') if os.getenv('DB_PASS') else envs['DB_PASS']))
       return conn
    except Exception as e:
       logging.error('Get Database PSQL, connection error', exc_info=True)
