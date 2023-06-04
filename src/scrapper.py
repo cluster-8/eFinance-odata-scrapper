c5b29084eef1a709460e6c3544e4e7ef4b834097
vinicius-hso <viniciushso@icloud.com>
2023-06-03 21:03:56 -0300

    Fix: run scrapper

--- src/scrapper.py
+++ src/scrapper.py
@@ -4,10 +4,16 @@ from .score import *
 from .historic_data import *
 
 def run_update_database():
-    print("Starting database update ...")
-    insert_consolidated_groups()
-    insert_financial_instituitions()
-    insert_physical_person_services()
-    insert_all_tariffs()
-    insert_logs()
-    print("Database updated successfully!")
+    try:
+        print("Starting database update ...")
+        insert_consolidated_groups()
+        insert_financial_instituitions()
+        insert_physical_person_services()
+        insert_juridical_person_services()
+        insert_all_tariffs()
+        print("Database updated successfully!")
+    except Exception as e:
+        print(f'Run Update Database error: {e}')
+    finally:
+        insert_logs()
+        
