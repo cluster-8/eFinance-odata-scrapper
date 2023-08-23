a7e0ce6a8dbdd6904770806f9ebb7dae7ee12535
vinicius-hso <viniciushso@icloud.com>
2023-08-23 19:49:41 -0300

    Fix: db connection env vars

--- src/db.py
+++ src/db.py
@@ -5,11 +5,6 @@ import logging
 
 envs = dotenv_values('./.env')
 
-print((os.getenv('DB_HOST') if os.getenv('DB_HOST') else envs['DB_HOST']))
-print((os.getenv('DB_NAME') if os.getenv('DB_NAME') else envs['DB_NAME']))
-print((os.getenv('DB_USER') if os.getenv('DB_USER') else envs['DB_USER']))
-print((os.getenv('DB_HOST') if os.getenv('DB_HOST') else envs['DB_HOST']))
-
 def get_database_psql():
    try:
       conn = psycopg2.connect(
